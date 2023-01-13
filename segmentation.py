7dd93f226fd8630c3aca9eece43adb40bb324b39
Nahidul Islam <97219701+analyticalnahid@users.noreply.github.com>
2023-01-13 14:27:55 +0600

    Add files via upload

---
+++ segmentation.py
@@ -0,0 +1,24 @@
+import torch
+from PIL import Image
+import io
+
+
+def get_yolov5():
+    # local best.pt
+ #   model_yolo = torch.hub.load('/yolov7', 'custom', './model/best.pt',force_reload='True')
+    model = torch.hub.load('./yolov5', 'custom', path='./model/yolov5n.pt', source='local')  # local repo
+    model.conf = 0.5
+    return model
+
+
+def get_image_from_bytes(binary_image, max_size=1024):
+    input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+    width, height = input_image.size
+    resize_factor = min(max_size / width, max_size / height)
+    resized_image = input_image.resize(
+        (
+            int(input_image.width * resize_factor),
+            int(input_image.height * resize_factor),
+        )
+    )
+    return resized_image
